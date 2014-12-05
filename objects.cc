fed8d6090f083ade48f5a89d2d4e84b432573594
Nate <gnate@rams.colostate.edu>
2014-12-05 12:54:02 -0700

    for lab

--- objects.cc
+++ objects.cc
@@ -120,9 +120,10 @@ ublas::vector<float> Face::getVertex(int i) {
 	
 ublas::vector<float> Face::getNormal() {
 	ublas::vector<float> e1 = p2 - p1;
-	ublas::vector<float> e2 = p3 - p1;
+	ublas::vector<float> e2 = p3 - p2;
 
-	return (1/norm_2(crossProductVectors(e1, e2))) * crossProductVectors(e1, e2);
+	//return (1/norm_2(crossProductVectors(e1, e2))) * crossProductVectors(e1, e2);
+	return crossProductVectors(e1, e2);
 }
 
 bool Face::isOnFace(ublas::vector<float> p) {
